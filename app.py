43ca8a39e82cd8c1f78505e906d68671306e2806
Pedro Luis Valdivieso <pedro@testserver>
2015-07-12 23:05:17 -0430

    Lab 2

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import *
 
-app = Flask (__name__, template_folder = 'views', static_folder = 'statics')
+app = Flask(__name__, template_folder = 'views', static_folder = 'statics')
 
 # Routes goes here
+
+@app.route('/')
+def index():
+	return 'Hello Flask'
+
+@app.route('/sayhello')
+def pedro():
+	name = request.args.get('name')
+	return render_template('say.html', name = name)
+
+@app.route('/form')
+def xs():
+	return render_template('form.html')
+
+@app.route('/login', methods = ['POST'])
+def olakase():
+	email = request.form['Email']
+	password = request.form['Password']
+	if email == 'pedro.valdivieso@gmail.com' and password == '123456':
+		return render_template('say.html', name = email)
+	return render_template('form.html')
+
+@app.route('/users')
+def users():
+	return render_template('users.html')
 
 # Routes end here
 
 if __name__ == '__main__':
   app.debug = True
   app.run( host = '0.0.0.0', port = 5000 )
-
-
